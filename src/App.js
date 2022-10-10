9f8d48dc5fb58d091e27abe024e17e7988e8337f
Michael <michaelbracey24@gmail.com>
2022-10-10 17:24:39 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
 
@@ -6,13 +6,23 @@ const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function fetchApi() {
+      let res = await fetch(pokeApi)
+      res = await res.json()
+      setData(res.results)
+    }
+    fetchApi()
+  }, [])
 
   return (
     <div data-testid="app">
       <Navigation />
-
-      <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+        {data.map(d => 
+          <PokemonCard key={d.name} url={d.url} name={d.name}/>
+        )}
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Card from 'react-bootstrap/Card';
 
 function PokemonCard({ url, name }) {
+  const [data, setData] = useState(null);
+
+  async function fetchApi() {
+    // let res = await fetch(url)
+    // res = await res.json()
+    // setData(res)
+    // currently rendering more than 150
+    console.log("response")
+  }
+
+  fetchApi()
 
   return (
-    <div>
-        pokemon card
-    </div>
+    data &&
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={data.sprites.front_default} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          {data.abilities.map(i => <li>{i.ability.name}</li>)}
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 
